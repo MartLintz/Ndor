e1be98b5039cbff091383e831e4bcf8fbc74d2e9
ArjunaKumarMohanta <lohitamohanta@gmail.com>
2022-11-12 13:36:38 +0530

    Initial commit

---
+++ app/src/main/java/com/arjuna/ndor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.arjuna.ndor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
